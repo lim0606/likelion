bccad278009c80edc0b45cd0b273bcfdc78fa632
Jaehyun Lim <lim0606@gmail.com>
2014-07-31 00:49:27 +0900

    w5_flaskr_mvc_sample is done

---
+++ w5_flaskr_sample/apps/flaskr_mvc.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# all the imports
+
+from flask import request, redirect, url_for,\
+    render_template
+from apps import app
+from database import Database
+
+from datetime import datetime
+
+dataStorage = Database()
+
+
+@app.route('/', methods=['GET', 'POST'])
+def show_entries():
+    entries = dataStorage.out()
+    return render_template('show_entries.html', entries=entries)
+
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    storage = {}
+    storage['title'] = request.form['title']
+    storage['contents'] = request.form['contents']
+    storage['datetime'] = datetime.now()
+    dataStorage.put(storage)
+    return redirect(url_for('show_entries'))
