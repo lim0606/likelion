4c2ea2fdd09d03451ced77921bb8eed0330b207c
Jaehyun Lim <lim0606@gmail.com>
2014-08-14 10:20:15 +0900

    w7_battleship debuging

--- w7_battleship/battleship.py
+++ w7_battleship/battleship.py
@@ -1,180 +1,195 @@
-record = 1
-Record = 1
-Board = 1
-board_size = 10
+def guess(record):
+    board_size = 10
 
+    class Ship:
+        # length = ?
+        # pos = [(x1,y1), (x2,y2), (x3, y3)]
+        # direction = 'vertical' or 'horizontal'
+        # id = 1, 2, 3, 4, or 5
+        # active = True
 
-class Ship:
-    # length = ?
-    # pos = [(x1,y1), (x2,y2), (x3, y3)]
-    # direction = 'vertical' or 'horizontal'
-    # id = 1, 2, 3, 4, or 5
-    # active = True
+        def __init__(self, id, x, y, direction, active):
+            self.id = id
+            self.active = active
+            if id < 3:
+                self.length = id + 1
+            else:
+                self.length = id
 
-    def __init__(self, id, x, y, direction, active):
-        self.id = id
-        self.active = active
-        if id < 3:
-            self.length = id + 1
-        else:
-            self.length = id
+            self.direction = direction
+            self.pos = [(x, y)]
+            if direction == 'vertical':
+                for i in range(1, self.length):
+                    self.pos.append((x, y + i))
+            elif direction == 'horizontal':
+                for i in range(1, self.length):
+                    self.pos.append((x + i, y))
+            else:
+                print "Error occurred in initialize Ship class object"
 
-        self.direction = direction
-        self.pos = [(x, y)]
-        if direction == 'vertical':
-            for i in range(1, self.length):
-                self.pos.append((x, y + i))
-        elif direction == 'horizontal':
-            for i in range(1, self.length):
-                self.pos.append((x + i, y))
-        else:
-            print "Error occurred in initialize Ship class object"
-
-
-def genShips(ship_id, active):
-    ship = Ship(1, 0, 0, 'horizontal', active)
-    ships = [ship]
-    ships_all = [ships]
-    ships_all.pop()
-
-    for id in ship_id:
-        ship = Ship(id, 0, 0, 'horizontal', active)
+    def genShips(ship_id, active):
+        ship = Ship(1, 0, 0, 'horizontal', active)
         ships = [ship]
-        ships.pop()
+        ships_all = [ships]
+        ships_all.pop()
 
-        if id < 3:
-            length = id + 1
-        else:
-            length = id
+        for id in ship_id:
+            ship = Ship(id, 0, 0, 'horizontal', active)
+            ships = [ship]
+            ships.pop()
 
-        direction = 'horizontal'
-        for x in range(0, board_size - length + 1):
-            for y in range(0, board_size):
-                ships.append(Ship(id, x, y, direction, active))
+            if id < 3:
+                length = id + 1
+            else:
+                length = id
 
-        direction = 'vertical'
-        for x in range(0, board_size):
-            for y in range(0, board_size - length + 1):
-                ships.append(Ship(id, x, y, direction, active))
+            direction = 'horizontal'
+            for x in range(0, board_size - length + 1):
+                for y in range(0, board_size):
+                    ships.append(Ship(id, x, y, direction, active))
 
-    return ships_all
+            direction = 'vertical'
+            for x in range(0, board_size):
+                for y in range(0, board_size - length + 1):
+                    ships.append(Ship(id, x, y, direction, active))
 
+        return ships_all
 
-log = record.get_latest()
+    log = record.get_latest()
 
-if len(log) == 0:  # if log equals to {} (empty dictionary)
-    mode = "hunt"
-
-    # the supplement matrix indicating each position has been missed or sank
-    visited = [0 for x in xrange(board_size * board_size)]
-
-else:
-    visited = record.data["visited"]
-
-    if log["result"] == Record.Status.MISSED:
-        visited[log["guess"]["x"] * board_size + log["guess"]["y"]] = 1
-    elif log["result"] == Record.Status.HIT:
-        mode = "target"
-    elif log["result"] == Record.Status.SINK:
+    if len(log) == 0:  # if log equals to {} (empty dictionary)
         mode = "hunt"
-        for x in range(0, board_size):
-            for y in range(0, board_size):
-                if record.get_status_at(x, y) == Board.Status.HIT:
-                    visited[x * board_size + y] = 1
-    elif log["result"] == Record.Status.WIN:
-        mode = "win"
-        print "I won!"
 
-# if log["result"] == Record.Status.MISSED or Record.Status.SINK:
-#     mode = "hunt"
-# elif log["result"] == Record.Status.HIT:
-#     mode = "target"
-# elif log["result"] == Record.Status.WIN:
-#     mode = "win"
-#     print "I won!"
+        # the supplement matrix indicating each position has been missed or
+        # sank
+        visited = [0 for x in xrange(board_size * board_size)]
+        record.data["visited"] = visited
+        record.data["mode"] = mode
+    else:
+        visited = record.data["visited"]
+        mode = record.data["mode"]
 
-if mode is "hunt":
-    # Step0: generate ships
-    # ship_id = record.get_remaining_ships()
-    # if len(log) == 0:  # if log equals to {} (empty dictionary)
-    #     print "Start the program!"
-    #     ships_all = genShips(ship_id)
-    # else:  # if there exist log before
-    #     ships_all = record.data["ship_id"]
+        if log["result"] == Record.Status.MISSED:
+            visited[log["guess"]["x"] * board_size + log["guess"]["y"]] = 1
+        elif log["result"] == Record.Status.HIT:
+            mode = "target"
+        elif log["result"] == Record.Status.SINK:
+            mode = "hunt"
+            for x in range(0, board_size):
+                for y in range(0, board_size):
+                    if record.get_status_at(x, y) == Board.Status.HIT:
+                        visited[x * board_size + y] = 1
+        elif log["result"] == Record.Status.WIN:
+            mode = "win"
+            print "I won!"
+        else:
+            print ":)"
 
-    # ship_id = [1, 2, 3, 4, 5]
-    ship_id = record.get_remaining_ships()
-    ships_all = genShips(ship_id, True)
+        record.data["visited"] = visited
+        record.data["mode"] = mode
 
-    # Step1: eliminate ships overlapped with missed positions
-    # Step2: eliminate ships overlapped with positions of other ships
-    # for id in ship_id:
-    #     if id < 3:
-    #         length = id + 1
-    #     else:
-    #         length = id
+    # if log["result"] == Record.Status.MISSED or Record.Status.SINK:
+    #     mode = "hunt"
+    # elif log["result"] == Record.Status.HIT:
+    #     mode = "target"
+    # elif log["result"] == Record.Status.WIN:
+    #     mode = "win"
+    #     print "I won!"
 
-    #     ships = ships_all[id]
-    for ships in ships_all:
-        for ship in ships:
-            for (x, y) in ship.pos:
-                if record.get_status_at(x, y) == Board.Status.HIT or record.get_status_at(x, y) == Board.Status.MISSED:
-                    ship.active = False
+    if mode == "hunt":
+        # Step0: generate ships
+        # ship_id = record.get_remaining_ships()
+        # if len(log) == 0:  # if log equals to {} (empty dictionary)
+        #     print "Start the program!"
+        #     ships_all = genShips(ship_id)
+        # else:  # if there exist log before
+        #     ships_all = record.data["ship_id"]
 
-    # get_status_at(x, y)
-    # get_remaining_ships()
-    # get_sink_info([ship_id])
-    # Step3: vote the positions of all remaining ships
-    board = [0 for x in xrange(board_size * board_size)]
+        # ship_id = [1, 2, 3, 4, 5]
+        ship_id = record.get_remaining_ships()
+        ships_all = genShips(ship_id, True)
 
-    for ships in ships_all:
-        for ship in ships:
-            if ship.active is True:
-                for i in ship.length:
-                    (x, y) = ship.pos[i]
-                    board[x * board_size + y] += 1
+        # Step1: eliminate ships overlapped with missed positions
+        # Step2: eliminate ships overlapped with positions of other ships
+        # for id in ship_id:
+        #     if id < 3:
+        #         length = id + 1
+        #     else:
+        #         length = id
 
-    index = sorted(range(len(board)), key=lambda k: board[k])
-    x = index % board_size
-    y = index / board_size
-    return x, y
-
-elif mode == "target":
-    ship_id = record.get_remaining_ships()
-    ships_all = genShips(ship_id, False)
-
-    # Step1: leave the ships overlapped with the only hits that are not
-    # related to sanked ships.
-    for ships in ships_all:
-        for ship in ships:
-            for (x, y) in ship.pos:
-                if record.get_status_at(x, y) == Board.Status.HIT and visited[x * board_size + y] == 0:
-                    ship.active = True
-
-    # Step2: eliminate ships overlapped with missed positions
-    for ships in ships_all:
-        for ship in ships:
-            if ship.active is True:
+        #     ships = ships_all[id]
+        for ships in ships_all:
+            for ship in ships:
                 for (x, y) in ship.pos:
-                    if record.get_status_at(x, y) == Board.Status.MISSED:
+                    if record.get_status_at(x, y) == Board.Status.HIT or record.get_status_at(x, y) == Board.Status.MISSED:
                         ship.active = False
 
-    # get_status_at(x, y)
-    # get_remaining_ships()
-    # get_sink_info([ship_id])
-    # Step3: vote the positions of all remaining ships
-    board = [0 for x in xrange(board_size * board_size)]
+        # get_status_at(x, y)
+        # get_remaining_ships()
+        # get_sink_info([ship_id])
+        # Step3: vote the positions of all remaining ships
+        board = [0 for x in xrange(board_size * board_size)]
 
-    for ships in ships_all:
-        for ship in ships:
-            if ship.active is True:
-                for i in ship.length:
-                    (x, y) = ship.pos[i]
-                    board[x * board_size + y] += 1
+        for ships in ships_all:
+            for ship in ships:
+                if ship.active is True:
+                    for i in ship.length:
+                        (x, y) = ship.pos[i]
+                        board[x * board_size + y] += 1
 
-elif mode == "win":
-    print "I won!"
-else:
-    print "Error occurred in overall logic"
+        index = sorted(range(len(board)), key=lambda k: board[k])
+        x = index[0] % board_size
+        y = index[0] / board_size
+        # return x, y
 
-record.data["visited"] = visited
+    elif mode == "target":
+        ship_id = record.get_remaining_ships()
+        ships_all = genShips(ship_id, False)
+
+        # Step1: leave the ships overlapped with the only hits that are not
+        # related to sanked ships.
+        for ships in ships_all:
+            for ship in ships:
+                for (x, y) in ship.pos:
+                    if record.get_status_at(x, y) == Board.Status.HIT and visited[x * board_size + y] == 0:
+                        ship.active = True
+
+        # Step2: eliminate ships overlapped with missed positions
+        for ships in ships_all:
+            for ship in ships:
+                if ship.active is True:
+                    for (x, y) in ship.pos:
+                        if record.get_status_at(x, y) == Board.Status.MISSED:
+                            ship.active = False
+
+        # get_status_at(x, y)
+        # get_remaining_ships()
+        # get_sink_info([ship_id])
+        # Step3: vote the positions of all remaining ships
+        board = [0 for x in xrange(board_size * board_size)]
+
+        for ships in ships_all:
+            for ship in ships:
+                if ship.active is True:
+                    for i in ship.length:
+                        (x, y) = ship.pos[i]
+                        board[x * board_size + y] += 1
+
+        index = sorted(range(len(board)), key=lambda k: board[k])
+        x = index[0] % board_size
+        y = index[0] / board_size
+        # return x, y
+
+    elif mode == "win":
+        print "I won!"
+        # return 0, 0
+        x = 0
+        y = 0
+    else:
+        print "Error occurred in overall logic"
+        # return 0, 0
+        x = 0
+        y = 0
+
+    # record.data["visited"] = visited
+    return x, y
