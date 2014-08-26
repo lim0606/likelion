41416165eca21b0e3f33011ed873c5675223fa38
Jaehyun Lim <lim0606@gmail.com>
2014-08-26 13:45:25 +0900

    w8_likelion-last-example v3 is done

--- w8_likelion-last-example/application/static/js/main.js
+++ w8_likelion-last-example/application/static/js/main.js
@@ -7,11 +7,13 @@ Pusher.log = function(message) {
 };
 */
 $(function() {
-    var pusher = new Pusher(PUSHER_KEY);
-    var testChannel = pusher.subscribe('test_channel');
-    $messages = $('.messages'); // new jQuery object having class 'messages'
-    $inputMessage = $('.inputMessage');
-    chatPage = $('.chat.page');
+    var pusher = new Pusher(PUSHER_KEY),
+	testChannel = pusher.subscribe('test_channel'),
+	broadcast = pusher.subscribe('br'),
+	$window = $(window),
+	$messages = $('.messages'), // new jQuery object having class 'messages'
+        $inputMessage = $('.inputMessage'),
+        chatPage = $('.chat.page');
 
     /*
     //$.post는 아래의 형태와 같습니다. 
@@ -37,17 +39,18 @@ $(function() {
 	addChatMessage({'username': 'Duhee', 'message': '??????!!!!!!!!!!!!'});
     }, initial_delay + 2000)
 
-    window.console.log("1asdfasdf");
+    // window.console.log("1asdfasdf");
     
-    testChannel.bind('echo', function(data) {
+    // testChannel.bind('echo', function(data) {
+    broadcast.bind('new_message', function(data) {
 	data['username'] = "Dongwoo";
 	addChatMessage(data);
     });
 
-    window.console.log("2asdfasdfasdf");
+    // window.console.log("2asdfasdfasdf");
     
-    setTimeout(function () {
-	window.console.log("asdf");
+/*    setTimeout(function () {
+	// window.console.log("asdf");
 	$.post('/api/echo', {"message": "Hello World!"});
     }, initial_delay + 4000)
     setTimeout(function () {
@@ -59,8 +62,8 @@ $(function() {
     setTimeout(function () {
 	$.post('/api/echo', {"message": "Hey Duhee bro! Please let me have some fried chicken!"});
     }, initial_delay + 7000)
-
-    window.console.log("3asdfasdf"); 
+*/
+    // window.console.log("3asdfasdf"); 
     
     function addChatMessage(data) {
 	var $usernameDiv = $('<span class="username"></span>');
@@ -98,4 +101,22 @@ $(function() {
 	var index = Math.abs(hash % 360);
 	return "hsl(" + index + ", 77%, 60%)";
     }
+
+    function sendMessage () {
+	var message = $inputMessage.val().trim();
+
+	// if ther is a non-empty message
+	if (message) {
+	    $inputMessage.val('');
+	    // $.post('/api/echo', {"message": message});
+	    $.post('/api/call/new_message', {"message": message});
+	}
+    }
+
+    $window.keydown(function(event) {
+	// When the client has ENTER on their keyboard
+	if (event.which == 13) {
+	    sendMessage();
+	}
+    });
 });
