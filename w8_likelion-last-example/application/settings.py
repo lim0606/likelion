53ec58172fbe3b97e1b8b5c125a3e6a38cbe8bc8
Jaehyun Lim <lim0606@gmail.com>
2014-08-27 10:49:42 +0900

    v6

--- w8_likelion-last-example/application/settings.py
+++ w8_likelion-last-example/application/settings.py
@@ -5,3 +5,9 @@ class Config(object):
 class Production(Config):
     DEBUG = False
     pass
+
+# set the secret key.  keep this really secret:
+import os
+# os.urandom(24)
+# app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+app.secret_key = os.urandom(24)
