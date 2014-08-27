5f8637fc680f5a7341d5370d5244c4955d40a96a
Jaehyun Lim <lim0606@gmail.com>
2014-08-27 10:50:16 +0900

    v6 is done

--- w8_likelion-last-example/application/__init__.py
+++ w8_likelion-last-example/application/__init__.py
@@ -3,4 +3,10 @@ from flask import Flask
 app = Flask('application')
 app.config.from_object('application.settings.Production')
 
+# set the secret key.  keep this really secret:
+import os
+# os.urandom(24)
+# app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+app.secret_key = os.urandom(24)
+
 import urls
--- w8_likelion-last-example/application/settings.py
+++ w8_likelion-last-example/application/settings.py
@@ -1,13 +1,6 @@
 class Config(object):
     pass
 
-
 class Production(Config):
     DEBUG = False
     pass
-
-# set the secret key.  keep this really secret:
-import os
-# os.urandom(24)
-# app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
-app.secret_key = os.urandom(24)
